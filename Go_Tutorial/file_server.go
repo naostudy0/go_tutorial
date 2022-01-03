99050edddb157fed8e5cccea66fbe08a8358b3ff
nao.u <nao.study0@gmail.com>
2022-01-04 02:54:52 +0900

    [add]別ページを表示するハンドラを追加

--- Go_Tutorial/file_server.go
+++ Go_Tutorial/file_server.go
@@ -3,13 +3,26 @@ package main
 import (
     "log"
     "net/http"
+    "text/template"
 )
+
+// テストページの表示
+func testHandler(w http.ResponseWriter, r *http.Request) {
+    t, err := template.ParseFiles("public/test.html")
+    if err != nil {
+        panic(err.Error())
+    }
+    if err := t.Execute(w, nil); err != nil {
+        panic(err.Error())
+    }
+}
 
 func main() {
     // 公開ディレクトリ
     fs := http.FileServer(http.Dir("public"))
     // URIとの対応
     http.Handle("/", fs)
+    http.HandleFunc("/test", testHandler)
     // サーバー起動（致命的なエラーが発生した場合はターミナルへ出力）
     log.Fatal(http.ListenAndServe(":8888", nil))
 }
